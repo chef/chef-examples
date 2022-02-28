1800176448189abce93a2574691bd9709ba32d0c
John S. Tonello <21206201+jtonello@users.noreply.github.com>
2022-02-28 10:20:23 -0500

    Create https_verify.rb
    Chef InSpec tests to verify the configuration of a secure web server.

---
+++ chef+ansible/tests/https_verify.rb
@@ -0,0 +1,16 @@
+describe port(443) do
+  it { should be_listening } 
+end
+
+describe http('https://localhost/', ssl_verify: false)do 
+  its('status') { should cmp 200 } 
+  its('body') { should match /Hello, world!/ } 
+end 
+
+describe ssl(port: 443).protocols('ssl3') do
+  it { should_not be_enabled }
+end
+
+describe ssl(port: 443).protocols('tls1.2') do
+  it { should be_enabled }
+end
